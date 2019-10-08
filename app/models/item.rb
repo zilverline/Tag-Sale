7801f4c88c0a810eca0889c09d491957c6e5857d
jamievullo <jvullo@protonmail.com>
2019-10-08 19:51:05 -0400

    First Commit

---
+++ app/models/item.rb
@@ -0,0 +1,6 @@
+class Item < ApplicationRecord
+
+    belongs_to :user 
+    belongs_to :category 
+    
+end
---
+++ db/migrate/20191008225555_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.boolean :new
+      t.text :description
+      t.integer :user_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
