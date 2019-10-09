71c83fac2e18ca5134ee6ca4c0644ecddd70db22
jamievullo <jvullo@protonmail.com>
2019-10-08 20:28:03 -0400

    Populated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Lucas", email: "test1@test.com", password: "test", location: "Taylor, PA")
+User.create(name: "Gabriel", email: "test2@test.com", password: "test", location: "Taylor, PA")
+User.create(name: "Scarlett", email: "test3@test.com", password: "test", location: "Taylor, PA")
+User.create(name: "Lucky", email: "test4@test.com", password: "test", location: "Scranton, PA")
+User.create(name: "Sunny", email: "test5@test.com", password: "test", location: "Dunmore, PA")
+User.create(name: "Tanner", email: "test6@test.com", password: "test", location: "Clarks Summit, PA")
+
