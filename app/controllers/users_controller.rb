17daec2bf62292dd145cc3fe867b2c64b3f86f00
jamievullo <jvullo@protonmail.com>
2019-10-08 21:31:40 -0400

    Added partial for user new and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            render :show
+        else
+            redirect_to new_user_path
+        end
+    end
+
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def edit 
+
+    end
+
+    def update
+
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :location)
+    end
 end
