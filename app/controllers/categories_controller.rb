9d2354ef8bd571f3488cb6721022d16fefc75e49
jamievullo <jvullo@protonmail.com>
2019-10-12 16:58:57 -0400

    Fixed category index page to display users and items selling in category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,18 +15,6 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
     end
 
-    def edit 
-
-    end
-
-    def update 
-
-    end
-
-    def destroy 
-
-    end
-
     private 
 
     def category_params 
