9c485c8cfa4ac13143227e253459c0ec0d2db39e
jamievullo <jvullo@protonmail.com>
2019-10-13 20:34:30 -0400

    Adding styling to forms and views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+    def alert_class(flash_type)
+        
+        case flash_type.to_s
+        when :notice
+            "alert success"
+        when :alert
+            "alert-warning"
+        when :error
+            "alert-danger"
+        end
+        
+    end
+
 end
